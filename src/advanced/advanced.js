d4d3a6c145382226032d84edd074d43bc918d57d
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-09-23 11:07:18 +0200

    finished

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -6,20 +6,63 @@ const nums = [1, 10, 3, 9, 4, 8, 5, 7, 6, 2, -5, -2, -4, -9] // eslint-disable-l
 let hasTen = false
 let indexOfTen = -1
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 10) {
+    hasTen = true
+    indexOfTen = i
+    break
+  }
+}
+
 // 2. Use a for loop to count how many numbers in the array are divisible by 3
 let divisibleByThreeCount = 0
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    divisibleByThreeCount++
+  }
+}
+
 // 3. use a for loop to find the average of the numbers in the array
 let average = 0
+let sum = 0
+
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+}
+
+average = sum / nums.length
 
 // 4. use a for loop to find the largest number in the array
 let largest = 0
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > largest) {
+    largest = nums[i]
+  }
+}
+
 // 5. use a for loop to find the smallest number in the array
 let smallest = 100000
 
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] < smallest) {
+    smallest = nums[i]
+  }
+}
+
 // 6. find the median of the numbers in the array
+
 let median = 0
+nums.sort((a, b) => a - b) // sort numerically
+
+const middle = Math.floor(nums.length / 2) // find middle index
+
+if (nums.length % 2 === 0) {
+  median = nums[middle - 1]
+} else {
+  median = nums[middle]
+}
 
 module.exports = {
   hasTen,
--- src/extensions/nested-loops.js
+++ src/extensions/nested-loops.js
@@ -13,24 +13,83 @@ const deepThree = []
 
 // HINT: in the below loop, the statements and block of code needs to be changed
 // HINT: in the below loop, the var i represents the loop index
-// for (let i = 5; i < 7; i++) {
-// Your code here
-// }
+
+for (let i = 1; i < 11; i++) {
+  simpleOne.push(i)
+}
 
 // 2. Using nested for loops, add arrays to 'nestedOne' where each array has n copies of the outer 'loop index'
 //    eg [[1],[2,2],...]
+for (let i = 1; i <= 10; i++) {
+  const arr = []
+  for (let x = 1; x <= i; x++) {
+    arr.push(i)
+  }
+  nestedOne.push(arr)
+}
 
 // 3. As 2, but each array should contain the values from the outer 'loop index' to 1 inclusive. Update array 'nestedTwo'
 //    eg [[1],[2,1],...]
+for (let i = 1; i <= 10; i++) {
+  const arr = []
+  for (let x = 1; x <= i; x++) {
+    arr.unshift(x)
+  }
+  nestedTwo.push(arr)
+}
 
 // 4. As 2, but each array should contain arrays from 1 to the outer 'loop index' with the value of the outer 'loop index'. Update array 'deepOne'
 //    eg [[[1]],[[2],[2,2]],...]
+for (let i = 1; i <= 10; i++) {
+  const arr = []
+  for (let x = 1; x <= i; x++) {
+    const arrDeep = []
+    for (let y = 1; y <= x; y++) {
+      arrDeep.push(i)
+    }
+    arr.push(arrDeep)
+  }
+  deepOne.push(arr)
+}
 
 // 5. As 4, update array 'deepTwo' so that the result is:
 //       [[[1]],[[1],[1,2]],...]
+for (let i = 1; i <= 10; i++) {
+  const arr = []
+  for (let x = 1; x <= i; x++) {
+    const arrDeep = []
+    for (let y = 1; y <= x; y++) {
+      arrDeep.push(y)
+    }
+    arr.push(arrDeep)
+  }
+  deepTwo.push(arr)
+}
 
 // 6. As 5, update the array 'deepThree', but the result should be the average of the sum of the squares of the numbers in each array
 //       [[1],[[1],[2.5]],...]
+for (let i = 1; i <= 10; i++) {
+  const outerArr = []
+  for (let x = 1; x <= i; x++) {
+    const innerArr = []
+    const sumArr = []
+
+    for (let y = 1; y <= x; y++) {
+      sumArr.push(y * y)
+    }
+
+    let sum = 0
+
+    for (let a = 0; a < sumArr.length; a++) {
+      sum += sumArr[a]
+    }
+
+    innerArr.push(sum / sumArr.length)
+    outerArr.push(innerArr)
+  }
+
+  deepThree.push(outerArr)
+}
 
 module.exports = {
   START,
